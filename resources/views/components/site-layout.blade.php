e112693604ea2c38f5af8032830064860a24f999
ClaudioFoti <clau.foti@gmail.com>
2023-02-26 15:54:54 +0100

    fix: fix contact form success toast text

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -34,7 +34,7 @@
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Check icon</span>
         </div>
-        <div class="ml-3 text-sm font-normal">{{ __(session('message')) }}</div>
+        <div class="ml-3 text-sm font-normal">{{ __(session('contact_form_success')) }}</div>
         <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-primary dark:hover:bg-primary-200 hover:bg-primary-700 rounded-lg focus:ring-2 p-1.5 inline-flex h-8 w-8" data-dismiss-target="#toast-success" aria-label="Close">
             <span class="sr-only">Close</span>
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
